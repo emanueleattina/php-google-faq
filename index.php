4cba22802800c86d07d73479d2df6a5b4fb03a2f
emanuele <emattina97@gmail.com>
2021-04-30 16:22:04 +0200

    Carico repo

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- link a fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous">
+
+    <!-- link a google font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <!-- link a mio css -->
+    <link rel="stylesheet" href="dist/css/master.css">
+
+    <title>Domande frequenti – Privacy e termini – Google</title>
+</head>
+<body>
+    <nav>
+
+    </nav>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html> 
