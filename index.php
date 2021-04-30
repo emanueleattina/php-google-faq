a94d1ef40e046092676eb7c8794ba1cee2c0f6b4
emanuele <emattina97@gmail.com>
2021-04-30 17:22:29 +0200

    Carico navbar

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php 
+    $faqs = [
+        [
+            [
+                'titolo' => 'Come state implementando la recente decisione della Corte di giustizia dell`Unione europea (CGUE) relativa al diritto all`oblio?',
+                'paragrafi' => [
+                    [
+
+                    ],
+                    [
+
+                    ],
+                ]
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- link a fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous">
+
+    <!-- link a google font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <!-- link a mio css -->
+    <link rel="stylesheet" href="dist/css/master.css">
+
+    <title>Domande frequenti – Privacy e termini – Google</title>
+</head>
+<body>
+    <nav>
+        <div class="container-fluid">
+            <div class="navbar-top">
+                <div class="navbar-left">
+                    <img src="dist/img/googlelogo_clr_74x24px.svg" alt="">
+                    <span><a>Privacy e termini</a></span>
+                </div>
+                <div class="navbar-right">
+                    <svg class="gb_We" focusable="false" viewBox="0 0 24 24">
+                        <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+                    </svg>
+                    <div class="avatar ml-20">
+                        <img src="https://loremflickr.com/250/250">
+                    </div>
+                </div>
+            </div>
+
+            <div class="navbar-bottom">
+                <ul>
+                    <li>Introduzione</li>
+                    <li>Norme sulla privacy</li>
+                    <li>Termini di servizio</li>
+                    <li>Tecnologie</li>
+                    <li class="active">Domande frequenti</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html> 
